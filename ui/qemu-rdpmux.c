e1d3210991fa7838afdf488b5ed4cd0b65ae3c7a
Sri Ramanujam <sramanujam@datto.com>
2016-05-23 17:37:38 -0400

    Experimental debug

--- ui/qemu-rdpmux.c
+++ ui/qemu-rdpmux.c
@@ -182,14 +182,17 @@ void mux_qemu_init(void)
     char uuid[64];
 
     if (qemu_uuid_set) {
+        printf("UUID set by QEMU command line!\n");
         snprintf(uuid, sizeof(uuid), UUID_FMT, qemu_uuid[0], qemu_uuid[1],
                 qemu_uuid[2], qemu_uuid[3], qemu_uuid[4], qemu_uuid[5],
                 qemu_uuid[6], qemu_uuid[7], qemu_uuid[8], qemu_uuid[9],
                 qemu_uuid[10], qemu_uuid[11], qemu_uuid[12], qemu_uuid[13],
                 qemu_uuid[14], qemu_uuid[15]);
         uuid_str = g_strdup(uuid);
+        printf("UUID is %s\n", uuid_str);
     } else {
         uuid_str = UUID_NONE;
+        printf("UUID was not set, defaulting TO UUID_NONE\n");
     }
     int id = g_random_int_range(0, INT_MAX);
 
